82c406274d5680cf0880d4213ad3f764954d86ad
Daniel Pecuch <daniel.pecuch@cleverlance.com>
2024-04-03 11:00:10 +0200

    Added more tests for each component

--- linguine-core/src/main/kotlin/com/qinshift/LinguineCore.kt
+++ linguine-core/src/main/kotlin/com/qinshift/LinguineCore.kt
@@ -1,7 +1,16 @@
 package com.qinshift
 
+import org.apache.tools.ant.taskdefs.Property
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 class LinguineCore : Plugin<Project> {
@@ -43,3 +52,39 @@ class LinguineCore : Plugin<Project> {
         }
     }
 }
+
+//abstract class GenerateLocalizationFileTask : DefaultTask() {
+//    @get:InputFile
+//    @get:PathSensitive(PathSensitivity.RELATIVE)
+//    abstract val inputFile: RegularFileProperty
+//
+//    @get:Input
+//    abstract val fileType: FileType
+//
+//    @get:Input
+//    abstract val minorDelimiter: String
+//
+//    @get:Input
+//    abstract val majorDelimiter: String
+//
+//    @get:OutputFile
+//    abstract val outputFile: RegularFileProperty
+//
+//    @TaskAction
+//    fun generate() {
+//        // Implementation remains as before but using properties
+//        val fileContent = FileReader().read(
+//            filePath = inputFile.get().asFile.absolutePath,
+//            fileType = fileType
+//        )
+//        val fileParser = FileParser(
+//            fileContent = fileContent,
+//            minorDelimiter = minorDelimiter,
+//            majorDelimiter = majorDelimiter
+//        )
+//        val root = fileParser.generateNestedMapStructureFromJSON()
+//        val outputFileContent = FileContentGenerator(fileContent).generateFileContent(root = root)
+//
+//        outputFile.get().asFile.writeText(outputFileContent.toString())
+//    }
+//}
