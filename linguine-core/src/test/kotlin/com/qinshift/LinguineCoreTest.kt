36d8985ca26fdabefd20878082f3e930aef4a745
Daniel Pecuch <daniel.pecuch@cleverlance.com>
2024-04-08 15:57:00 +0200

    Fux LinguineCoreTest

--- linguine-core/src/test/kotlin/com/qinshift/LinguineCoreTest.kt
+++ linguine-core/src/test/kotlin/com/qinshift/LinguineCoreTest.kt
@@ -1,35 +1,39 @@
-//package com.qinshift
-//
-//import org.gradle.api.Project
-//import org.gradle.testfixtures.ProjectBuilder
-//import org.junit.jupiter.api.Assertions.assertEquals
-//import org.junit.jupiter.api.Assertions.assertNotNull
-//import org.junit.jupiter.api.Test
-//
-//class LinguineCoreTest {
-//
-//    @Test
-//    fun pluginRegistersATask() {
-//        // Create a test project and apply the plugin
-//        val project: Project = ProjectBuilder.builder().build()
-//        project.plugins.apply("com.qinshift.linguine")
-//
-//        // Verify the result
-//        assertNotNull(project.tasks.findByName("loc"))
-//    }
-//
-//    @Test
-//    fun pluginShouldBeConfigured() {
-//        val project: Project = ProjectBuilder.builder().build()
-//
-//        project.pluginManager.apply("com.qinshift.linguine")
-//        val extension = project.extensions.getByType(LinguineConfig::class.java)
-//        extension.jsonFilePath = "src/commonMain/resources/string.json"
-//        extension.outputDirPath = "presentation"
-//        extension.stringsFileName = "Strings.kt"
-//
-//        assertEquals("src/commonMain/resources/string.json", extension.jsonFilePath)
-//        assertEquals("presentation", extension.outputDirPath)
-//        assertEquals("Strings.kt", extension.stringsFileName)
-//    }
-//}
+package com.qinshift
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+class LinguineCoreTest {
+
+    @Test
+    fun pluginRegistersATask() {
+        // Create a test project and apply the plugin
+        val project: Project = ProjectBuilder.builder().build()
+        project.plugins.apply("com.qinshift.linguine")
+        val extension = project.extensions.getByType(LinguineConfig::class.java)
+        extension.inputFilePath = "src/commonMain/resources/string.json"
+        extension.outputFilePath = "presentation"
+        extension.outputFileName = "Strings.kt"
+
+        // Verify the result
+        assertNotNull(project.tasks.findByName("loc"))
+    }
+
+    @Test
+    fun pluginShouldBeConfigured() {
+        val project: Project = ProjectBuilder.builder().build()
+
+        project.pluginManager.apply("com.qinshift.linguine")
+        val extension = project.extensions.getByType(LinguineConfig::class.java)
+        extension.inputFilePath = "src/commonMain/resources/string.json"
+        extension.outputFilePath = "presentation"
+        extension.outputFileName = "Strings.kt"
+
+        assertEquals("src/commonMain/resources/string.json", extension.inputFilePath)
+        assertEquals("presentation", extension.outputFilePath)
+        assertEquals("Strings.kt", extension.outputFileName)
+    }
+}
