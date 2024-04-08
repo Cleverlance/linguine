b2dda50b014eacc749eb0791a0dfcb65e0cba388
Daniel Pecuch <daniel.pecuch@cleverlance.com>
2024-04-08 15:24:42 +0200

    Correct FileWriter tests

--- linguine-core/src/functionalTest/kotlin/com/qinshift/FileWriterTest.kt
+++ linguine-core/src/functionalTest/kotlin/com/qinshift/FileWriterTest.kt
@@ -1,25 +1,49 @@
 package com.qinshift
 
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
+import java.io.File
 import java.nio.file.Path
-import kotlin.test.Test
+import kotlin.test.BeforeTest
 
 class FileWriterTest {
+
+	private lateinit var fileWriter: FileWriter
+
+	@TempDir
+	lateinit var tempDir: Path
+
+	@BeforeTest
+	fun setUp() {
+		fileWriter = FileWriter()
+	}
+
+	private fun assertFileContentMatches(file: File, expectedContent: String) {
+		file.exists() shouldBe true
+		val readContent = file.readText()
+		readContent shouldBe expectedContent
+	}
+
 	@Test
-	fun `writeToFile creates directories and writes content correctly`(@TempDir tempDir: Path) {
-		val fileWriter = FileWriter()
-		val testContent = StringBuilder("This is a test.")
-		val nestedFilePath = tempDir.resolve("nested/dir/testOutput.txt").toFile()
+	fun `writeToFile writes correct text to the file`() {
+		val testFile = File(tempDir.toFile(), "testFileWriter.kt")
+		val testContent = StringBuilder("This is a test string to be written\n.")
 
-		nestedFilePath.parentFile.exists() shouldBe false
+		fileWriter.writeToFile(testFile, testContent)
 
-		fileWriter.writeToFile(nestedFilePath.absolutePath, testContent)
+		assertFileContentMatches(testFile, testContent.toString())
+	}
 
-		nestedFilePath.parentFile.exists() shouldBe true
+	@Test
+	fun `writeToFile should create file if it does not exist`() {
+		val nonExistentFile = File(tempDir.toFile(), "nonexistent/subdirectory/testFile.txt")
+		val contentToWrite = StringBuilder("Test content")
 
-		val writtenContent = nestedFilePath.readText()
+		nonExistentFile.exists() shouldBe false
 
-		testContent.toString() shouldBe writtenContent
+		fileWriter.writeToFile(nonExistentFile, contentToWrite)
+
+		assertFileContentMatches(nonExistentFile, contentToWrite.toString())
 	}
 }
