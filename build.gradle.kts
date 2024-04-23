918ae4c2a881e6e70ef73b4e2196c7a6cfca59e9
Bořek Leikep <borek.leikep@cleverlance.com>
2024-04-23 17:02:08 +0200

    Publishing to Maven Central using in memory keys.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,65 @@
+import com.vanniktech.maven.publish.MavenPublishPlugin
+import com.vanniktech.maven.publish.SonatypeHost
 import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.dokka.gradle.DokkaPlugin
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.gradle.buildconfig) apply false
+    alias(libs.plugins.gradle.maven.publish) apply true
     alias(libs.plugins.detekt) apply true
-    `maven-publish`
+    alias(libs.plugins.dokka) apply false
 }
 
 subprojects {
+    apply<DokkaPlugin>()
     apply<MavenPublishPlugin>()
 
     group = "io.github.cleverlance.linguine"
-    version = "0.1.0-SNAPSHOT"
+    version = "0.1.0"
 
-    publishing {
-        repositories {
-            maven {
-                url = uri("https://nexus.cleverlance.com/nexus/repository/maven-snapshots/")
-                credentials {
-                    username = System.getenv("NEXUS_USERNAME")
-                    password = System.getenv("NEXUS_PASSWORD")
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+        signAllPublications()
+
+        pom {
+            name = "Linguine"
+            description = "Simplifies the localization process in Kotlin projects."
+            inceptionYear = "2024"
+            url = "https://github.com/cleverlance/linguine/"
+            licenses {
+                license {
+                    name = "The MIT License"
+                    url = "https://github.com/cleverlance/linguine/blob/main/license.md"
+                    distribution = "https://github.com/cleverlance/linguine/blob/main/license.md"
                 }
+            }
+            developers {
+                developer {
+                    id = "RealDanie1"
+                    name = "Daniel Pecuch"
+                    url = "https://github.com/RealDanie1"
+                }
+                developer {
+                    id = "RadekKuzel"
+                    name = "Radek Kůžel"
+                    url = "https://github.com/RadekKuzel"
+                }
+                developer {
+                    id = "JiriHromek"
+                    name = "Jiří Hromek"
+                    url = "https://github.com/JiriHromek"
+                }
+                developer {
+                    id = "gerak-cz"
+                    name = "Bořek Leikep"
+                    url = "https://github.com/gerak-cz"
+                }
+            }
+            scm {
+                url = "https://github.com/cleverlance/linguine/"
+                connection = "scm:git:git://github.com/cleverlance/linguine.git"
+                developerConnection = "scm:git:ssh://git@github.com:cleverlance/linguine.git"
             }
         }
     }
