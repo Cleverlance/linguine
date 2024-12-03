01a300cb4cef4ba0706e652de6372af944ba7a2b
Lukas Lizal <lukas.lizal@cleverlance.com>
2024-12-03 09:10:45 +0100

    Remove unused iOS run script phase

--- examples/single-module-multiplatform-app/shared/src/commonMain/kotlin/com/qinshift/project/Greeting.kt
+++ examples/single-module-multiplatform-app/shared/src/commonMain/kotlin/com/qinshift/project/Greeting.kt
@@ -7,10 +7,10 @@ class Greeting {
     private val platform = getPlatform()
 
     fun greet(greetingNumber: Int): String {
-        return Demo.hello("${greetingNumber}", platform.name)
+        return Demo.hello("$greetingNumber", platform.name)
     }
 
     fun alternativeGreet(greetingNumber: Int): String {
-        return Localiser.localise("demo__hello", "${greetingNumber}", platform.name)
+        return Localiser.localise("demo__hello", "$greetingNumber", platform.name)
     }
 }
