bbc7583edb4b0f48fcac7501f6ecdb0170194973
Lukas Lizal <lukas.lizal@cleverlance.com>
2024-12-09 14:42:56 +0100

    Update AN screen example

--- sample/single-module-multiplatform-app/composeApp/src/androidMain/kotlin/com/qinshift/project/App.kt
+++ sample/single-module-multiplatform-app/composeApp/src/androidMain/kotlin/com/qinshift/project/App.kt
@@ -4,15 +4,20 @@ package com.qinshift.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import presentation.Credits
+import presentation.Demo
 
 import single_module_multiplatform_app.composeapp.generated.resources.Res
 import single_module_multiplatform_app.composeapp.generated.resources.compose_multiplatform
@@ -24,7 +29,7 @@ fun App() {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+                Text(Demo.buttonLabel)
             }
             AnimatedVisibility(showContent) {
                 val firstGreeting = remember { Greeting().greet(1) }
@@ -37,6 +42,11 @@ fun App() {
                     Text("Compose: $thirdGreeting")
                 }
             }
+            Spacer(Modifier.weight(1f))
+            Text(
+                Credits.info,
+                modifier = Modifier.padding(16.dp)
+            )
         }
     }
 }
