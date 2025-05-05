6b5c76f1af49148e42f0f34e58cb71bfeefc5d67
Daniel Pecuch <daniel.pecuch@cleverlance.com>
2025-05-05 18:58:28 +0200

    Fix FileParser wrongly generating file names:
    - "two_factor_authentication__message" generated as Two_factor_authentication
    - newly "two_factor_authentication__message" should generate a file with name TwoFactorAuthentication

--- linguine-generator/src/main/kotlin/com/qinshift/linguine/linguinegenerator/FileParser.kt
+++ linguine-generator/src/main/kotlin/com/qinshift/linguine/linguinegenerator/FileParser.kt
@@ -8,11 +8,20 @@ class FileParser(
     fun generateGroupedMapStructure(): Map<String, Map<String, Any>> {
         val groupedMap = mutableMapOf<String, MutableMap<String, Pair<String, String>>>()
         fileContent.forEach { (key, value) ->
-            val firstSegment =
-                key.substringBefore(majorDelimiter).replaceFirstChar { it.uppercaseChar() }
-            val restOfKey = key.substringAfter(majorDelimiter)
-            groupedMap.computeIfAbsent(firstSegment) { mutableMapOf() }[restOfKey] = key to value
+            val groupName: String
+            val nestedKey: String
+
+            if (key.contains(majorDelimiter)) {
+                groupName = key.substringBefore(majorDelimiter).toPascalCase(minorDelimiter)
+                nestedKey = key.substringAfter(majorDelimiter)
+            } else {
+                groupName = key.toPascalCase(minorDelimiter)
+                nestedKey = key
+            }
+
+            groupedMap.computeIfAbsent(groupName) { mutableMapOf() }[nestedKey] = key to value
         }
+
         return groupedMap.mapValues { (_, map) -> generateNestedMapStructure(map) }
     }
 
@@ -51,11 +60,14 @@ class FileParser(
         }
     }
 
-    private fun formatPart(part: String, isLast: Boolean): String {
-        return if (isLast) {
+    private fun formatPart(part: String, isIntermediate: Boolean): String {
+        return if (isIntermediate) {
             part.replaceFirstChar { it.uppercaseChar() }
         } else {
             part.replaceFirstChar { it.lowercase() }
         }
     }
+
+    private fun String.toPascalCase(splitBy: String): String =
+        this.split(splitBy).joinToString("") { it.replaceFirstChar { c -> c.uppercaseChar() } }
 }
